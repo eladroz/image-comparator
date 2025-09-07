7707a5082351bc4eb1c01c35734748d8be486709
Elad Rosenheim <elad.rosenheim@gmail.com>
2025-09-07 14:49:15 +0300

    Fix Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import netlify from '@netlify/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlify()],
+  plugins: [react(), tailwindcss(), netlify()],
 })
